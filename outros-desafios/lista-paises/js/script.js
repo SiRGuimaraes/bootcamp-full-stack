55731a5004b82a2a85b03161801aac22d2a69d7f
Rafael <rafaelgnogueira@hotmail.com>
2021-03-16 21:18:10 -0300

    renderCountryList, renderFavorites implemented

--- outros-desafios/lista-paises/js/script.js
+++ outros-desafios/lista-paises/js/script.js
@@ -36,5 +36,41 @@ function render(){
   handleCountryButtons();
 }
 
+function renderCountryList(){
+  let countriesHtml = '';
+
+  allCountries.forEach(country => countriesHtml += handleHtmlList(country));
+
+  tabCountries.innerHTML = countriesHtml;
+}
+
+function renderFavorites(){
+  let favoritesHtml = '';
+
+  favoriteCountries.forEach(country => favoritesHtml += handleHtmlList(country));
+
+  tabFavorites.innerHTML = favoritesHtml;
+
+  const buttons = tabFavorites.querySelectorAll('.btn');
+  buttons.forEach((button)=>{
+    button.classList.add('red', 'darken-4');
+    button.textContent = '-';
+  });
+}
+
+function handleHtmlList(country){
+  const {id, name, population, flag} = country;
+  const a = `<a id="${id}" class="waves-effect waves-light btn">+</a>`;
+  const img = `<img src="${flag}" alt="${name}">`;
+  const ul = `<ul><li>${name}</li><li>${formatNumber(population)}</li></ul>`
+  
+  return countryDiv = `
+  <div class='country'>
+    <div>${a}</div>
+    <div>${img}</div>
+    <div>${ul}</div>
+  </div>
+  `
+}
 
 start();
