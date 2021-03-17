ac9e74848120721dc22d35d4c09b94390a552fe7
Rafael <rafaelgnogueira@hotmail.com>
2021-03-16 21:01:08 -0300

    state variables created

---
+++ outros-desafios/lista-paises/js/script.js
@@ -0,0 +1,9 @@
+const tabCountries = document.querySelector('#tabCountries');
+const tabFavorites = document.querySelector('#tabFavorites');
+const countCountries = document.querySelector('#countCountries');
+const countFavorites = document.querySelector('#countFavorites');
+const totalPopulationList = document.querySelector('#totalPopulationList');
+const totalPopulationFavorites = document.querySelector('#totalPopulationFavorites');
+
+let allCountries = [];
+let favoriteCountries = [];
