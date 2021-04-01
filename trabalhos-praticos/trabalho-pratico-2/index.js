884e93f09c551b531f606dd6d9630e65504e5d26
Rafael <rafaelgnogueira@hotmail.com>
2021-04-01 10:39:45 -0300

    method getCitiesNames created

--- trabalhos-praticos/trabalho-pratico-2/index.js
+++ trabalhos-praticos/trabalho-pratico-2/index.js
@@ -1,19 +1,24 @@
 import { promises } from 'fs';
+
 const { readFile, writeFile } = promises;
-const ALL_STATES = 'origin-json/Estados.json';
-const ALL_CITIES = 'origin-json/Cidades.json';
+const STATES_FILE = JSON.parse(await readFile('origin-json/Estados.json'));
+const CITIES_FILE = JSON.parse(await readFile('origin-json/Cidades.json'));
+const STATE_CITIES_FILE = async (uf) =>
+  JSON.parse(await readFile(`states/${uf}.json`));
 
 async function start() {
-  await createFiles();
-  await getStatesCitiesCount();
+  //await createFiles();
+  //await getStatesCitiesCount();
+  //await getStatesCitiesCount(false);
+  await getCitiesNames();
+  await getCitiesNames(false);
 }
 
 async function createFiles() {
-  const states = JSON.parse(await readFile(ALL_STATES));
-  const cities = JSON.parse(await readFile(ALL_CITIES));
+  const states = STATES_FILE;
+  const cities = CITIES_FILE;
 
-  for (let state of states) {
-    const { ID, Sigla } = state;
+  for (let { ID, Sigla } of states) {
     const stateCities = cities.filter((city) => city.Estado === ID);
     await writeFile(
       `states/${Sigla}.json`,
@@ -23,16 +28,15 @@ async function createFiles() {
 }
 
 async function getCitiesCount(uf) {
-  const cities = JSON.parse(await readFile(`states/${uf}.json`));
+  const cities = await STATE_CITIES_FILE(uf);
   return cities.length;
 }
 
-async function getStatesCitiesCount(withMoreCities) {
-  const states = JSON.parse(await readFile(ALL_STATES));
+async function getStatesCitiesCount(withMoreCities = true) {
+  const states = STATES_FILE;
   const statesList = [];
 
-  for (let state of states) {
-    const { Sigla } = state;
+  for (let { Sigla } of states) {
     const citiesCount = await getCitiesCount(Sigla);
     statesList.push({ uf: Sigla, citiesCount });
   }
@@ -50,6 +54,41 @@ async function getStatesCitiesCount(withMoreCities) {
     });
   }
   console.log(topFive);
+}
+
+async function getCitiesNames(withBiggerNames = true) {
+  const states = STATES_FILE;
+  const citiesList = [];
+
+  for (let { Sigla } of states) {
+    let cityName;
+
+    cityName = await getBiggerOrSmallerName(Sigla, withBiggerNames);
+
+    citiesList.push(`${cityName} - ${Sigla}`);
+  }
+  console.log(citiesList);
+}
+
+async function getBiggerOrSmallerName(uf, biggerName = true) {
+  const cities = await STATE_CITIES_FILE(uf);
+  let cityName = cities[0].Nome;
+
+  for (let { Nome } of cities) {
+    if (biggerName) {
+      if (Nome.length > cityName.length) {
+        cityName = Nome;
+      }
+    } else {
+      if (Nome.length < cityName.length) {
+        cityName = Nome;
+      }
+    }
+    if (Nome.localeCompare(cityName) === -1) {
+      cityName = Nome;
+    }
+  }
+  return cityName;
 }
 
 start();
