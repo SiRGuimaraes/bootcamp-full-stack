ab4ad8a4097cc4c00932cca04a6a9310cd17fbe4
Rafael <rafaelgnogueira@hotmail.com>
2021-03-31 22:58:24 -0300

    method getStatesCitiesCount created

--- trabalhos-praticos/trabalho-pratico-2/index.js
+++ trabalhos-praticos/trabalho-pratico-2/index.js
@@ -1,10 +1,11 @@
-import { promises, read } from 'fs';
+import { promises } from 'fs';
 const { readFile, writeFile } = promises;
 const ALL_STATES = 'origin-json/Estados.json';
 const ALL_CITIES = 'origin-json/Cidades.json';
 
 async function start() {
   await createFiles();
+  await getStatesCitiesCount();
 }
 
 async function createFiles() {
@@ -24,6 +25,31 @@ async function createFiles() {
 async function getCitiesCount(uf) {
   const cities = JSON.parse(await readFile(`states/${uf}.json`));
   return cities.length;
+}
+
+async function getStatesCitiesCount(withMoreCities) {
+  const states = JSON.parse(await readFile(ALL_STATES));
+  const statesList = [];
+
+  for (let state of states) {
+    const { Sigla } = state;
+    const citiesCount = await getCitiesCount(Sigla);
+    statesList.push({ uf: Sigla, citiesCount });
+  }
+  statesList.sort((a, b) => b.citiesCount - a.citiesCount);
+
+  const topFive = [];
+
+  if (withMoreCities) {
+    statesList.slice(0, 5).forEach(({ uf, citiesCount }) => {
+      topFive.push(`${uf} - ${citiesCount}`);
+    });
+  } else {
+    statesList.slice(-5).forEach(({ uf, citiesCount }) => {
+      topFive.push(`${uf} - ${citiesCount}`);
+    });
+  }
+  console.log(topFive);
 }
 
 start();
